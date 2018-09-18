2b30267d41d203a19e5600d200912276fe429179
Raaja Gouthaman <rgouthaman@tavisca.com>
2018-09-18 19:04:27 +0530

    WCF ASSIGNMNET

---
+++ HotelsProduct/Hotel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelsProduct
+{
+    [DataContract]
+    public class Hotel
+    {
+        [DataMember]
+        public int HotelId;
+        [DataMember]
+        public string HotelName;
+        [DataMember]
+        public string Address;
+        [DataMember]
+        public int StarRating;
+        [DataMember]
+        public int Price;
+    }
+}
